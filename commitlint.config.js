2c0e28de5d45955cf69ad64855689dc82152c2dd
shubin <shubin.zhai@gmail.com>
2023-02-03 20:02:52 -0600

    chore1:added commitlint:added husky config

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,7 @@ module.exports = {
   "rules": {
     "header-max-length": [0, "always", 72],
     "body-empty":[1, "never",0],
-    "subject-empty":[0, "never",0],
+    "subject-empty":[1, "never",0],
     "type-empty":[0, "never",0]
 
   }
